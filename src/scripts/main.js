a404cbb013ef97ff2a81d7643e5b83ae94b29bc0
Sean Tracey <seanmtracey@gmail.com>
2016-03-24 15:25:17 +0000

    Removed timeout, added network issues dialog

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,38 +21,44 @@ const chromeStorage = {
 	}
 };
 
+function updateIDs() {
+	[].slice.call(document.querySelectorAll('.screen-id')).forEach(function(el) {
+		el.innerHTML = viewer.getData('id');
+	});
+}
+
 const view = document.querySelector('webview');
 
 window.onload = function() {
 	
-	setTimeout(function(){
-		
-		const Viewer = require('ftlabs-screens-viewer');
-		const viewer = new Viewer('http://ftlabs-screens.herokuapp.com', chromeStorage);
-		// const viewer = new Viewer('http://local.ft.com:8080', chromeStorage);
-		viewer.start();
-		
-		viewer.on('change', e => {
-			console.log('change', e);
-			view.src = e;
-		});
+	const Viewer = require('ftlabs-screens-viewer');
+	const viewer = new Viewer('http://ftlabs-screens.herokuapp.com', chromeStorage);
+	viewer.start();
+	
+	viewer.on('change', e => {
+		console.log('change', e);
+		view.src = e;
+	});
 
-		// A reload has been forced
-		viewer.on('reload', e => {
-			console.log('reload', e);
-			view.reload();
-		});
-
-		// E.g. The viewer has started but cannot connected to the server.
-		viewer.on('not-connected', e => {
-			console.log('n-connected', e);
-		});
-		
-		viewer.on('ready', e => {
-			console.log("Viewer ready");
-			view.src = e;
-		})
-		
-	}, 3000);
+	// A reload has been forced
+	viewer.on('reload', e => {
+		console.log('reload', e);
+		view.reload();
+	});
+	
+	// E.g. The viewer has started but cannot connected to the server.
+	viewer.on('not-connected', e => {
+		console.log('n-connected', e);
+		document.body.classList.add('network-issue');
+	});
+	
+	viewer.on('ready', e => {
+		console.log("Viewer ready");
+		view.src = e;
+	});
+	
+	viewer.on('id-change', function () {
+		updateIDs();
+	});
 
 }
