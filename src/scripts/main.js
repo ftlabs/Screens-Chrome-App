7122c1d99216f768c5910b7d46e526914fc03edf
Ada Rose Edwards <ada@ada.is>
2016-04-03 11:52:58 -0700

    Port iframe buffering logic into webviews for chrome extension

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,20 @@
 
 /*global window, chrome, document*/
 
+
 const chromeStorage = {
 	setItem : function(storageKey, data, callback){
 		const storageObj = {};
 		storageObj[storageKey] = JSON.stringify(data);
-		
-		chrome.storage.local.set( storageObj, callback )
+
+		chrome.storage.local.set( storageObj, callback );
 	},
 	getItem : function(storageKey, callback){
 		chrome.storage.local.get(storageKey, function(d){
 			if(Object.keys(d).length === 0){
 				callback(null);
 			} else {
-				d = JSON.parse(d[storageKey])
+				d = JSON.parse(d[storageKey]);
 				callback(d);
 			}
 		});
@@ -22,101 +23,170 @@ const chromeStorage = {
 };
 
 const bringDown = (function(){
-	
+
 	const bootTime = new Date();
-	let targetHour = 24;	
+	let targetHour = 24;
 	let restartTime;
 	let bringDownSet = false;
-	
+
 	function setTimeUntilBD(){
-		
+
 		if(bringDownSet){
 			console.warn("The bring-down has already been set. You cannot reset it. This system will restart in %d seconds", restartTime);
 			return;
 		}
-		
+
 		// If the targetHour has already passed in the day, the next opportunity is the next day
 		// So, here, figure out how long we have to wait until the same time tomorrow.
 		if(targetHour < bootTime.getHours()){
 			targetHour = 24 - (bootTime.getHours() - targetHour);
 		}
-		
+
 		const hoursUntilTarget = ((targetHour - bootTime.getHours()) * 60 * 60) * 1000;
 		const minutesFromNextHour = ((60 - bootTime.getMinutes()) * 60) * 1000;
-		
+
 		const millisecondsUntilEarliestPossibleRestart = hoursUntilTarget - minutesFromNextHour;
 		const giveALittle = (60 * 60 * 1000) * Math.random() | 0;
-		
+
 		const restartIn = millisecondsUntilEarliestPossibleRestart + giveALittle;
-		
+
 		setTimeout(function(){
-			
+
 			chrome.runtime.restart();
-					
+
 		}, restartIn);
-		
+
 		bringDownSet = true;
 		restartTime = Date.now() + restartIn;
-		
-		// The unix time that the Chrome OS device will be restarted at. 
+
+		// The unix time that the Chrome OS device will be restarted at.
 		return restartTime;
-		
+
 	}
-	
+
 	function getTimeUntilBD(){
-		
+
 		return restartTime;
-		
+
 	}
-	
+
 	return {
 		set : setTimeUntilBD,
 		get : getTimeUntilBD
 	};
-	
-})();
 
-const view = document.querySelector('webview');
+}());
+
+const views = [].slice.call(document.querySelectorAll('webview'));
+
+function webViewLoaded() {
+	const currentActive = document.querySelector('webView.active');
+	if (currentActive) kickOutIframe(currentActive);
+	this.classList.remove('buffering');
+	this.classList.add('active');
+	this.removeEventListener('load', webViewLoaded);
+}
+
+function kickOutIframe(webView) {
+	webView.classList.remove('active');
+	webView.classList.remove('buffering');
+	webView.classList.add('done');
+	setTimeout(() => webView.src = 'about:blank', 500);
+	webView.removeEventListener('contentload', webViewLoaded);
+
+	// remove self from the list
+	usedViews.splice(usedViews.indexOf(webView), 1);
+}
+
+function prepareIframetoLoad(webView, url) {
+	usedViews.push(webView);
+	webView.classList.add('buffering');
+	webView.classList.remove('done');
+	webView.src = url;
+	webView.addEventListener('contentload', webViewLoaded);
+}
+
+const usedViews = [];
+function updateUrl(url) {
+	if (!url) {
+		return;
+	}
+
+	// another url has been added
+	if (usedViews.length < views.length) {
+		const nextIframe = views.filter(webView => usedViews.indexOf(webView) === -1)[0];
+		prepareIframetoLoad(nextIframe, url);
+		return;
+	}
+
+	// a third has been added kick up the first one so the next one can load
+	if (usedViews.length === views.length) {
+		const next = usedViews[0];
+		kickOutIframe(next);
+		prepareIframetoLoad(next, url);
+
+		console.log('Forced Load');
+
+		// load the next webView regardless
+		webViewLoaded.bind(usedViews[0])();
+		return;
+	}
+}
 
 window.onload = function() {
-	
+
 	const Viewer = require('ftlabs-screens-viewer');
+	const Carousel = require('ftlabs-screens-carousel');
+	let carousel;
 	const viewer = new Viewer('http://ftlabs-screens.herokuapp.com', chromeStorage);
-	
+
 	function updateIDs() {
 		[].slice.call(document.querySelectorAll('.screen-id')).forEach(function(el) {
 			el.innerHTML = viewer.getData('id');
 		});
-	}	
-	
-	viewer.on('change', e => {
-		console.log('change', e);
-		view.src = e;
+	}
+
+	viewer.on('change', url => {
+		console.log('change', url);
+
+		if (carousel) {
+			// stop timers
+			carousel.destroy();
+			carousel = null;
+		}
+
+		if (Carousel.isCarousel(url)) {
+			carousel = new Carousel(url, 'http://ftlabs-screens.herokuapp.com');
+			carousel.on('change', updateUrl);
+			updateUrl(carousel.getCurrentURL());
+		} else {
+			updateUrl(url);
+		}
 	});
 
 	// A reload has been forced
 	viewer.on('reload', e => {
 		console.log('reload', e);
-		view.reload();
+		views.forEach(view => view.reload());
 	});
-	
+
 	// E.g. The viewer has started but cannot connected to the server.
 	viewer.on('not-connected', e => {
 		console.log('n-connected', e);
 		document.body.classList.add('network-issue');
 	});
-	
+
 	viewer.on('ready', e => {
 		console.log("Viewer ready");
-		view.src = e;
+		updateUrl(e);
 	});
-	
+
 	viewer.on('id-change', function () {
 		updateIDs();
 	});
-	
+
 	viewer.start();
-	
+
 	bringDown.set();
-	
-}
+
+};
